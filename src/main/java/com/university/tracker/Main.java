23c00d18a9b56efb1c4fae0ce3bf2250a9d62074
Manuel Alejandro Herrera Uribe <manuel.herrera01@globant.com>
2026-03-10 16:36:33 -0500

    fix : Duplicate access modifiers
    Se me chispoteo cuando solucione conflictos pero ahora si

--- src/main/java/com/university/tracker/Main.java
+++ src/main/java/com/university/tracker/Main.java
@@ -10,10 +10,6 @@ public class Main {
     private static List<UniversityClass> classes = new ArrayList<>();
     private static Scanner sc = new Scanner(System.in);
 
-    private static List<Teacher> teachers = new ArrayList<>();
-    private static List<Student> students = new ArrayList<>();
-    private static List<UniversityClass> classes = new ArrayList<>();
-
     public static void main(String[] args) {
         initializeData();
         showMenu();
